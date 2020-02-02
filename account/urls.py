605c589f78f94626ed1602f74f4007b8320b3d6c
amir lesani <xenups@outlook.com>
2020-02-02 19:06:18 +0330

    add token login to api url

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from account import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('register/', views.RegisterUserView.as_view()),
+    # path('register/', views.RegisterUserView.as_view()),
     path('validation/', views.PhoneValidationView.as_view()),
-    path('login/', views.LoginView.as_view()),
-    path('hello/', views.HelloWorld.as_view()),
+    path('otplogin/', views.LoginView.as_view()),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
